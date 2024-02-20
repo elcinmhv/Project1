a6b28ec1fd04bb126ac1c083bf536f708d9972fb
elcinmhv <elcinmhrrmov150@gmail.com>
2024-02-20 22:47:05 +0400

    modeller

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from core.models import Category
+from core.models import Category,Product,Color
 # Register your models here.
 
 admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Color)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+# from django.utils import timezone
+from datetime import datetime
+
+
+SIZE=(
+    ('XS','Extra Small'),
+    ('S','Small'),
+    ('M','Medium'),
+    ('L','Large'),
+    ('XL','Extra Large')
+)
 
 # Create your models here.
 class BaseModel(models.Model):
@@ -9,5 +20,41 @@ class BaseModel(models.Model):
     class Meta:
         abstract=True
 
+
 class Category(BaseModel):
+    name=models.CharField(max_length=50 )
+    class Meta:
+        verbose_name='Category'
+        verbose_name_plural='Categories'
+   
+    
+
+    def __str__(self):
+        formatted_date = datetime.strftime(self.created_at, '%y,%m,%d-%H:%M:%S')
+        return f'{self.name}-{formatted_date}'
+        # return f"{self.name} - {timezone.localtime(self.created_at)}"
+class Color(BaseModel):
     name=models.CharField(max_length=50)
+    class Meta:
+        verbose_name='Color'
+        verbose_name_plural='Colors'
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(BaseModel):
+    name=models.CharField(max_length=50)
+    price=models.FloatField()
+    color=models.ManyToManyField(Color)
+    size=models.CharField(max_length=50,choices=SIZE,default='M')
+    like=models.IntegerField(default=0)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name='Product'
+        verbose_name_plural='Products'
+
+    def __str__(self):
+        return self.name
